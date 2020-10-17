37556c169a44464e6569a926cebbd54987080b72
nobbyfix <nobbyfix@users.noreply.github.com>
2020-10-17 22:16:13 +0200

    add more constants

--- PythonScripts/Constants.py
+++ PythonScripts/Constants.py
@@ -1,18 +1,19 @@
 import os, json
 
 RARITY_LETTER = [ 'E', 'E', 'B', 'P', 'G', 'R' ]
-RARITY_NAME = [ 'Normal', 'Normal', 'Rare', 'Elite', 'Super Rare', 'Legendary' ]
+RARITY_NAME_SHIP = [ 'Normal', 'Normal', 'Rare', 'Elite', 'Super Rare', 'Ultra Rare' ]
+RARITY_NAME_ITEM = [ 'Normal', 'Normal', 'Rare', 'Elite', 'Super Rare', 'Legendary' ]
 RARITY_NAME_RESEARCH = {4: 'Priority', 5: 'Decisive'}
 
 with open(os.path.join('data', 'item_wiki_filenames.json')) as jfile:
 	ITEMFILENAMES = json.load(jfile)
 
 ITEMNAME_REPLACE = {
-		'Mystery': 'Random',
-		'Series ': 'S',
-		'General Blueprint -': 'Strengthening Unit',
-		'樱之御守': 'Sakura Amulets',
-		'荣誉之冠': 'Miniature Crowns'
+	'Mystery': 'Random',
+	'Series ': 'S',
+	'General Blueprint -': 'Strengthening Unit',
+	'樱之御守': 'Sakura Amulets',
+	'荣誉之冠': 'Miniature Crowns'
 }
 
 def item_name(name:str) -> str:
@@ -31,3 +32,35 @@ def item_link(name:str) -> str:
 
 def item_filename(name:str) -> str:
 	return ITEMFILENAMES.get(name)
+
+
+NATIONALITY = {
+	0: 'Universal',
+	1: 'Eagle Union',
+	2: 'Royal Navy',
+	3: 'Sakura Empire',
+	4: 'Ironblood',
+	5: 'Eastern Radiance',
+	6: 'Sardegna Empire',
+	7: 'North Union',
+	8: 'Iris Libre',
+	9: 'Vichya Dominion',
+	98: 'Universal',
+	101: 'Neptunia',
+	102: 'Bilibili',
+	103: 'Utawarerumono',
+	104: 'Kizuna AI',
+	105: 'Hololive',
+}
+
+AMRMOR_TYPE = {
+	1: 'Light',
+	2: 'Medium',
+	3: 'Heavy',
+}
+
+SKILL_TYPE = {
+	1: 'Offense',
+	2: 'Defense',
+	3: 'Support',
+}
