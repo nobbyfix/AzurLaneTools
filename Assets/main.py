0e876ce19da956f1f0eb1460d69ab67b55bf3baf
nobbyfix <nobbyfix@users.noreply.github.com>
2021-06-17 20:05:46 +0200

    basically just added comments

--- Assets/main.py
+++ Assets/main.py
@@ -7,26 +7,28 @@ from lib.classes import Client
 
 
 def main(client: Client):
+	# load config data from files
 	userconfig = config.load_user_config()
 	clientconfig = config.load_client_config(client)
 
-	CLIENT_ASSET_DIR = Path('ClientAssets', client.name)
-	if not CLIENT_ASSET_DIR.parent.exists():
-		CLIENT_ASSET_DIR.parent.mkdir(parents=True)
+	CLIENT_ASSET_DIR = Path("ClientAssets", client.name)
+	if not CLIENT_ASSET_DIR.exists():
+		CLIENT_ASSET_DIR.mkdir(parents=True)
 
 	version_response = protobuf.get_version_response(clientconfig.gateip, clientconfig.gateport)
-	versionlist = [versioncontrol.parse_version_string(v) for v in version_response.pb.Version if v.startswith('$')]
+	versionlist = [versioncontrol.parse_version_string(v) for v in version_response.pb.Version if v.startswith("$")]
 	for vresult in versionlist:
 		updater.update(vresult, clientconfig.cdnurl, userconfig.useragent, CLIENT_ASSET_DIR)
 
 if __name__ == "__main__":
+	# setup argument parser
 	parser = ArgumentParser()
-	parser.add_argument('client', metavar='CLIENT', type=str, help='client to update')
+	parser.add_argument("client", metavar="CLIENT", type=str, help="client to update")
 	args = parser.parse_args()
 
+	# parse arguments
 	clientname = args.client
-	if clientname in Client._member_names_:
-		client = Client[clientname]
-		main(client)
-	else:
-		sys.exit(f'The client {clientname} is not supported or does not exist.')
+	if not clientname in Client._member_names_:
+		sys.exit(f"The client {clientname} is not supported or does not exist.")
+	client = Client[clientname]
+	main(client)
