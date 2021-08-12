4a42b9a08c022119bdef8a3350a0a489a04d9af9
nobbyfix <nobbyfix@users.noreply.github.com>
2021-08-12 16:34:01 +0200

    retry downloads after timeout (can still hang if it keeps timing out)

--- Assets/lib/downloader.py
+++ Assets/lib/downloader.py
@@ -1,11 +1,24 @@
 import requests
 
+
 def download_hashes(cdnurl, versionhash, useragent):
-	response = requests.get(f'{cdnurl}/android/hash/{versionhash}', headers={'user-agent': useragent})
+	while True:
+		try:
+			response = requests.get(f'{cdnurl}/android/hash/{versionhash}', headers={'user-agent': useragent}, timeout=30)
+			break
+		except TimeoutError:
+			print("Hash request timed out, retrying.")
+
 	if response.status_code == 200:
 		return response.text
 
 def download_asset(cdnurl, filehash, useragent):
-	response = requests.get(f'{cdnurl}/android/resource/{filehash}', headers={'user-agent': useragent})
+	while True:
+		try:
+			response = requests.get(f'{cdnurl}/android/resource/{filehash}', headers={'user-agent': useragent}, timeout=20)
+			break
+		except TimeoutError:
+			print("Asset request timed out, retrying.")
+
 	if response.status_code == 200:
 		return response.content
